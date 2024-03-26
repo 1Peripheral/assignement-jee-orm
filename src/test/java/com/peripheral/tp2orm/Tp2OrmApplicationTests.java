8d5e5f6c72ae4c12690f14cbd9d9c9650c40764c
1Peripheral <codels@protonmail.com>
2024-03-26 16:09:33 +0000

    Initial Commit

---
+++ src/test/java/com/peripheral/tp2orm/Tp2OrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peripheral.tp2orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2OrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
